d2c55da4ec66c7620a3db780d29b33928a568517
Stefan Kubala <stevokubala4445@gmail.com>
2024-08-26 11:49:27 +0200

    responsivity

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,36 @@
 function Header() {
   return (
-    <div className="header">
-      <div className="navbar">
-        <div className="logo">
-          <img
-            src={
-              "https://mtobaly.sitecloud.sk/wp-content/uploads/elementor/thumbs/mtm-logo-qse7b4y7w10mhvg31d7rldcigeuh6si7s055be2874.png"
-            }
-            alt="logo"
-          ></img>
-        </div>
-        <nav>
-          <ul>
-            <li>
-              <a href="#">FEFCO katalóg</a>
-            </li>
-            <li>
-              <a href="https://mtobaly.sitecloud.sk/portfolio-produktov/">
-                Portfólio-test
-              </a>
-            </li>
-            <li>
-              <a href="https://mtobaly.sitecloud.sk/o-nas/">O nás</a>
-            </li>
-            <li>
-              <a href="https://mtobaly.sitecloud.sk/kontakt/">Kontakt</a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </div>
+    // <div className="header">
+    //   <div className="navbar">
+    //     <div className="logo">
+    //       <img
+    //         src={
+    //           "https://mtobaly.sitecloud.sk/wp-content/uploads/elementor/thumbs/mtm-logo-qse7b4y7w10mhvg31d7rldcigeuh6si7s055be2874.png"
+    //         }
+    //         alt="logo"
+    //       ></img>
+    //     </div>
+    //     <nav>
+    //       <ul>
+    //         <li>
+    //           <a href="#">FEFCO katalóg</a>
+    //         </li>
+    //         <li>
+    //           <a href="https://mtobaly.sitecloud.sk/portfolio-produktov/">
+    //             Portfólio-test
+    //           </a>
+    //         </li>
+    //         <li>
+    //           <a href="https://mtobaly.sitecloud.sk/o-nas/">O nás</a>
+    //         </li>
+    //         <li>
+    //           <a href="https://mtobaly.sitecloud.sk/kontakt/">Kontakt</a>
+    //         </li>
+    //       </ul>
+    //     </nav>
+    //   </div>
+    // </div>
+    <p>test</p>
   );
 }
 
